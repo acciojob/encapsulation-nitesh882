15e54126325d7c3a33aacaa357d0a73874a1f994
Nitesh Badiya <101646890+nitesh882@users.noreply.github.com>
2024-08-14 09:34:06 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
+// Main.java
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly(); // Create an object of RWOnly
+        
+        // Use setter to set the value of 'name'
+        obj.setName("John");
+
+        // Use getter to access the value of 'name'
+        System.out.println(obj.getName());
+    }
 }
